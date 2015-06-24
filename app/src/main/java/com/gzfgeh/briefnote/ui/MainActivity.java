26c16411e605f62a2b339b0bd84b92892a141788
gzfgeh <gzfgeh@sina.com>
2015-06-24 20:17:28 +0800

    init theme

---
+++ app/src/main/java/com/gzfgeh/briefnote/ui/MainActivity.java
@@ -0,0 +1,105 @@
+package com.gzfgeh.briefnote.ui;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.gzfgeh.briefnote.R;
+import com.gzfgeh.briefnote.utils.ThemeUtils;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initStatusWindow();
+        initToolBar();
+        initTheme();
+    }
+
+    private void initStatusWindow(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintColor(getColorPrimary());
+            tintManager.setStatusBarTintEnabled(true);
+        }
+    }
+
+    private void initToolBar(){
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(getColorPrimary());
+        toolbar.setTitleTextColor(getResources().getColor(R.color.action_bar_title_color));
+        toolbar.collapseActionView();
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        invalidateOptionsMenu();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+                toolbar.setTitle(R.string.app_name);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+                toolbar.setTitle(R.string.app_name);
+            }
+        };
+        drawerToggle.syncState();
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerLayout.setScrimColor(getResources().getColor(R.color.drawer_scrim_color));
+    }
+
+    private void initTheme(){
+        ThemeUtils.Theme theme = ThemeUtils.Theme.mapValueToTheme(0);
+        ThemeUtils.changTheme(this, theme);
+    }
+
+    public int getColorPrimary(){
+        TypedValue typedValue = new  TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        return typedValue.data;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
