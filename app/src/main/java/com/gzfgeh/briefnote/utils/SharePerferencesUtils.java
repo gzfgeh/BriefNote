06d9c5563c5a288509a236504f96ffe04bc925ed
gzfgeh <gzfgeh@sina.com>
2015-07-20 15:26:43 +0800

    change SharePreference

--- app/src/main/java/com/gzfgeh/briefnote/utils/SharePerferencesUtils.java
+++ app/src/main/java/com/gzfgeh/briefnote/utils/SharePerferencesUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
+import com.gzfgeh.briefnote.APP;
+
 public class SharePerferencesUtils {
     public final static String SETTING = "Setting";
     
@@ -11,60 +13,98 @@ public class SharePerferencesUtils {
     	
     }
     
-    public static void putValue(Context context,int resKey, int value) {
-         Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();  
+    public static void putValue(int resKey, int value) {
+         Context context = APP.getContext();
+         Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
          sp.putInt(context.getString(resKey), value);
          sp.commit();  
     }  
-    public static void putValue(Context context,int resKey, boolean value) {
+    public static void putValue(int resKey, boolean value) {
+        Context context = APP.getContext();
          Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();  
          sp.putBoolean(context.getString(resKey), value);
          sp.commit();  
     }
 
-    public static void putValue(Context context,int resKey, String value) {
+    public static void putValue(int resKey, String value) {
+        Context context = APP.getContext();
          Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();  
          sp.putString(context.getString(resKey), value);
          sp.commit();  
     }
 
-    public static void putValue(Context context,String resKey, long value) {
+    public static void putValue(int resKey, long value) {
+        Context context = APP.getContext();
+        Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
+        sp.putLong(context.getString(resKey), value);
+        sp.commit();
+    }
+
+    public static void putValue(String resKey, long value) {
+        Context context = APP.getContext();
         Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
         sp.putLong(resKey, value);
         sp.commit();
     }
 
-    public static int getValue(Context context,int resKey, int defValue) {
+    public static void putValue(String resKey, int value) {
+        Context context = APP.getContext();
+        Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
+        sp.putInt(resKey, value);
+        sp.commit();
+    }
+
+    public static void putValue(String resKey, boolean value) {
+        Context context = APP.getContext();
+        Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
+        sp.putBoolean(resKey, value);
+        sp.commit();
+    }
+
+    public static void putValue(String resKey, String value) {
+        Context context = APP.getContext();
+        Editor sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE).edit();
+        sp.putString(resKey, value);
+        sp.commit();
+    }
+
+    public static int getValue(int resKey, int defValue) {
+        Context context = APP.getContext();
         SharedPreferences sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);  
         int value = sp.getInt(context.getString(resKey), defValue);
         return value;  
     }
 
-    public static boolean getValue(Context context,int resKey, boolean defValue) {
+    public static boolean getValue(int resKey, boolean defValue) {
+        Context context = APP.getContext();
         SharedPreferences sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);  
         boolean value = sp.getBoolean(context.getString(resKey), defValue);
         return value;  
     }
 
-    public static String getValue(Context context,int resKey, String defValue) {
+    public static String getValue(int resKey, String defValue) {
+        Context context = APP.getContext();
         SharedPreferences sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);  
         String value = sp.getString(context.getString(resKey), defValue);
         return value;  
     }
 
-    public static int getValue(Context context,String resKey, int defValue) {
+    public static int getValue(String resKey, int defValue) {
+        Context context = APP.getContext();
         SharedPreferences sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
         int value = sp.getInt(resKey, defValue);
         return value;
     }
 
-    public static String getValue(Context context,String resKey, String defValue) {
+    public static String getValue(String resKey, String defValue) {
+        Context context = APP.getContext();
         SharedPreferences sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
         String value = sp.getString(resKey, defValue);
         return value;
     }
 
-    public static long getValue(Context context,String resKey, long defValue) {
+    public static long getValue(String resKey, long defValue) {
+        Context context = APP.getContext();
         SharedPreferences sp =  context.getSharedPreferences(SETTING, Context.MODE_PRIVATE);
         long value = sp.getLong(resKey, defValue);
         return value;
